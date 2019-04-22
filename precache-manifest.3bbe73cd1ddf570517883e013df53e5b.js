ca088db07772ab86a8740b4fb846919100829d48
Scott Fodje <sfodje@turnitin.com>
2019-04-22 16:23:46 -0700

    Updates

---
+++ precache-manifest.3bbe73cd1ddf570517883e013df53e5b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2915d175f4a8d2d043b6f9db4dd479",
+    "url": "/happy-thoughts/index.html"
+  },
+  {
+    "revision": "df946426fffefbdfc7c3",
+    "url": "/happy-thoughts/static/css/2.6e697513.chunk.css"
+  },
+  {
+    "revision": "14bfd63e95766ad13069",
+    "url": "/happy-thoughts/static/css/main.5a34e481.chunk.css"
+  },
+  {
+    "revision": "df946426fffefbdfc7c3",
+    "url": "/happy-thoughts/static/js/2.bd2458ce.chunk.js"
+  },
+  {
+    "revision": "14bfd63e95766ad13069",
+    "url": "/happy-thoughts/static/js/main.85f23f1a.chunk.js"
+  },
+  {
+    "revision": "b4e1c817acf8ef5d4f9f",
+    "url": "/happy-thoughts/static/js/runtime~main.95b75753.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/happy-thoughts/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/happy-thoughts/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/happy-thoughts/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/happy-thoughts/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/happy-thoughts/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/happy-thoughts/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/happy-thoughts/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/happy-thoughts/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/happy-thoughts/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/happy-thoughts/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/happy-thoughts/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "a9049bfde37ee18a676918a9929217a9",
+    "url": "/happy-thoughts/static/media/logo.a9049bfd.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/happy-thoughts/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/happy-thoughts/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/happy-thoughts/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/happy-thoughts/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/happy-thoughts/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
